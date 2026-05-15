c135e98d68c336a8126256150debe8f0baf5ff9e
HuyFat <huyn32368@gmail.com>
2026-05-15 14:59:56 +0700

    fix deploy

--- QuanLyNhanVien/Program.cs
+++ QuanLyNhanVien/Program.cs
@@ -1,43 +1,45 @@
 using Microsoft.EntityFrameworkCore;
-using QuanLyNhanVien.Data;
+using Microsoft.AspNetCore.HttpOverrides;
 using OfficeOpenXml;
+using QuanLyNhanVien.Data;
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 var builder = WebApplication.CreateBuilder(args);
 
-// EPPlus license
 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
-// MVC
 builder.Services.AddControllersWithViews();
 
-// DB (Supabase PostgreSQL)
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        npgsqlOptions =>
-        {
-            npgsqlOptions.EnableRetryOnFailure();
-        }));
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// SignalR
 builder.Services.AddSignalR();
 
-// PORT Render
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://*:{port}");
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
 
-// Exception handling
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor |
+        ForwardedHeaders.XForwardedProto
+});
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-
-
 
 app.UseStaticFiles();
 
@@ -45,15 +47,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-// Custom middleware
-app.UseMiddleware<QuanLyNhanVien.Middleware.MobileAccessMiddleware>();
-
-// Default route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Employee}/{action=Index}/{id?}");
-
-// SignalR hub
-app.MapHub<QuanLyNhanVien.Hubs.NotificationHub>("/notificationHub");
 
 app.Run();
